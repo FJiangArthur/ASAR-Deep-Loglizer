aa3ecf956bfc2a177b615f21811436a7daa55f10
cranberrymuffin <aparna_natarajan@brown.edu>
2021-12-03 13:37:50 -0500

    new model: vae encoder lstm decoder

---
+++ demo/vae_lstm_train.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import random
+import sys
+sys.path.append("../")
+import argparse
+
+import tensorflow as tf
+from models.vae_lstm import LSTM
+
+from deeploglizer.common.preprocess import FeatureExtractor
+from deeploglizer.common.dataloader import load_sessions
+from deeploglizer.common.utils import seed_everything, dump_params, dump_final_results
+
+
+parser = argparse.ArgumentParser()
+
+##### Model params
+parser.add_argument("--model_name", default="Autoencoder", type=str)
+parser.add_argument("--hidden_size", default=128, type=int)
+parser.add_argument("--num_directions", default=2, type=int)
+parser.add_argument("--num_layers", default=2, type=int)
+parser.add_argument("--embedding_dim", default=32, type=int)
+
+##### Dataset params
+parser.add_argument("--dataset", default="HDFS", type=str)
+parser.add_argument(
+    "--data_dir", default="../data/processed/HDFS_100k/hdfs_1.0_tar", type=str
+)
+parser.add_argument("--window_size", default=10, type=int)
+parser.add_argument("--stride", default=1, type=int)
+
+##### Input params
+parser.add_argument("--feature_type", default="sequentials", type=str, choices=["sequentials", "semantics"])
+parser.add_argument("--use_tfidf", action="store_true")
+parser.add_argument("--max_token_len", default=50, type=int)
+parser.add_argument("--min_token_count", default=1, type=int)
+# Uncomment the following to use pretrained word embeddings. The "embedding_dim" should be set as 300
+# parser.add_argument(
+#     "--pretrain_path", default="../data/pretrain/wiki-news-300d-1M.vec", type=str
+# )
+
+##### Training params
+parser.add_argument("--epoches", default=100, type=int)
+parser.add_argument("--batch_size", default=1024, type=int)
+parser.add_argument("--learning_rate", default=0.01, type=float)
+parser.add_argument("--anomaly_ratio", default=0.1, type=float)
+parser.add_argument("--patience", default=3, type=int)
+
+##### Others
+parser.add_argument("--random_seed", default=42, type=int)
+parser.add_argument("--gpu", default=0, type=int)
+parser.add_argument("--cache", default=False, type=bool)
+
+params = vars(parser.parse_args())
+
+model_save_path = dump_params(params)
+
+class tf_data_generator(tf.keras.utils.Sequence):
+        def __init__(self, session_dict, batch_size, feature_type="semantics", shuffle=True):
+            self.shuffle = shuffle,
+            self.max_input_size = 0
+            self.batch_size = batch_size,
+            flatten_data_list = []
+            self.feature_len = 0
+            # flatten all sessions
+            for session_idx, data_dict in enumerate(session_dict.values()):
+                features = data_dict["features"][feature_type]
+                window_labels = data_dict["window_labels"]
+                window_anomalies = data_dict["window_anomalies"]
+
+                # This is for making the input work
+                self.max_input_size = max(self.max_input_size, len(window_labels))
+                for window_idx in range(len(window_labels)):
+                    sample = {
+                        "session_idx": session_idx,  # not session id
+                        "features": features[window_idx],
+                        "window_labels": window_labels[window_idx],
+                        "window_anomalies": window_anomalies[window_idx],
+                    }
+                    self.feature_len = len(features[window_idx])
+                    flatten_data_list.append(sample)
+            self.flatten_data_list = flatten_data_list
+
+        def on_epoch_end(self):
+            if self.shuffle:
+                random.shuffle(self.flatten_data_list)
+
+        def __getitem__(self, idx):
+            batches = self.flatten_data_list[idx * self.batch_size[0]:(idx + 1) * self.batch_size[0]]
+            return batches
+
+        def __len__(self):
+            return len(self.flatten_data_list) // self.batch_size[0]
+
+if __name__ == "__main__":
+    seed_everything(params["random_seed"])
+
+    session_train, session_test = load_sessions(data_dir=params["data_dir"])
+    ext = FeatureExtractor(**params)
+
+    session_train = ext.fit_transform(session_train)
+    session_test = ext.transform(session_test, datatype="test")
+
+    seed_everything(params["random_seed"])
+
+    session_train, session_test = load_sessions(data_dir=params["data_dir"])
+    ext = FeatureExtractor(**params)
+
+    session_train = ext.fit_transform(session_train)
+    session_test = ext.transform(session_test, datatype="test")
+    dataset_train = tf_data_generator(session_train, feature_type=params["feature_type"],
+                                      batch_size=params["batch_size"], shuffle=True)
+    dataset_test = tf_data_generator(session_test, feature_type=params["feature_type"], batch_size=params["batch_size"],
+                                     shuffle=True)
+
+    curr_batch_size = 1024
+    # our noise dimension
+
+    # create generator and discriminator
+    # ext.meta_data = {'num_labels': 14, 'vocab_size': 14}
+    max_input_senquence_len = dataset_train.max_input_size
+    feature_len = dataset_train.feature_len
+    hidden_size = 256
+    num_layers = 2
+    num_keys = ext.meta_data['vocab_size']
+    emb_dimension = 128
+
+    num_labels = ext.meta_data['num_labels']
+    model = LSTM(meta_data=ext.meta_data, batch_sz=curr_batch_size, model_save_path=model_save_path, **params)
+
+    eval_results = model.fit(
+        train_loader = dataset_train,
+        test_loader=dataset_test,
+        epoches=params["epoches"],
+    )
+
+    dump_final_results(params, eval_results, model)
---
+++ models/vae_lstm.py
@@ -0,0 +1,124 @@
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Reshape, ReLU
+from tensorflow.math import exp, sqrt, square
+from models.tf_basemodel import tf_BasedModel
+
+
+def reparametrize(mu, logvar):
+    """
+    Differentiably sample random Gaussian data with specified mean and variance using the
+    reparameterization trick.
+
+    Suppose we want to sample a random number z from a Gaussian distribution with mean mu and
+    standard deviation sigma, such that we can backpropagate from the z back to mu and sigma.
+    We can achieve this by first sampling a random value epsilon from a standard Gaussian
+    distribution with zero mean and unit variance, then setting z = sigma * epsilon + mu.
+
+    For more stable training when integrating this function into a neural network, it helps to
+    pass this function the log of the variance of the distribution from which to sample, rather
+    than specifying the standard deviation directly.
+
+    Inputs:
+    - mu: Tensor of shape (N, Z) giving means
+    - logvar: Tensor of shape (N, Z) giving log-variances
+
+    Returns:
+    - z: Estimated latent vectors, where z[i, j] is a random value sampled from a Gaussian with
+         mean mu[i, j] and log-variance logvar[i, j].
+    """
+    ################################################################################################
+    # TODO: Reparametrize by initializing epsilon as a normal distribution and scaling by          #
+    # posterior mu and sigma to estimate z                                                         #
+    ################################################################################################
+    # Replace "pass" statement with your code
+    sigma = sqrt(exp(logvar))
+    epsilon = tf.random.normal(sigma.shape)
+    z = sigma * epsilon + mu
+    ################################################################################################
+    #                              END OF YOUR CODE                                                #
+    ################################################################################################
+    return z
+
+class LSTM(tf_BasedModel):
+    def __init__(
+        self,
+        meta_data,
+        batch_sz,
+        hidden_size=100,
+        num_directions=2,
+        num_layers=1,
+        window_size=None,
+        use_attention=False,
+        embedding_dim=16,
+        model_save_path="./lstm_models",
+        feature_type="sequentials",
+        label_type="next_log",
+        eval_type="session",
+        topk=5,
+        use_tfidf=False,
+        freeze=False,
+        gpu=-1,
+        **kwargs
+    ):
+        super().__init__(
+            meta_data=meta_data,
+            batch_sz=batch_sz,
+            model_save_path=model_save_path,
+            feature_type=feature_type,
+            label_type=label_type,
+            eval_type=eval_type,
+            topk=topk,
+            use_tfidf=use_tfidf,
+            embedding_dim=embedding_dim,
+            freeze=freeze,
+            gpu=gpu,
+            **kwargs
+        )
+        self.num_labels = meta_data["num_labels"]
+
+        self.feature_type = feature_type
+        self.label_type = label_type
+        self.hidden_size = hidden_size
+        self.num_directions = num_directions
+        self.use_tfidf = use_tfidf
+        self.embedding_dim = embedding_dim
+
+        self.encoder = Sequential(layers=[
+            Dense(self.embedding_dim, activation="relu"),
+            Dense(self.embedding_dim, activation="relu"),
+            Dense(self.embedding_dim, activation="relu"),
+        ])
+        self.mu_layer = Dense(self.embedding_dim)
+        self.logvar_layer = Dense(self.embedding_dim)
+
+        self.lstm = tf.keras.layers.LSTM(self.hidden_size)
+
+        self.linear1 = Dense(
+            self.hidden_size // 2, activation='relu')
+        self.linear2 = Dense(
+            self.num_labels, activation=None,
+        )
+
+
+    def call(self, input_dict):
+        if self.label_type == "anomaly":
+            y = tf.convert_to_tensor(input_dict["window_anomalies"])
+        elif self.label_type == "next_log":
+            y = tf.convert_to_tensor(input_dict["window_labels"])
+
+        y = tf.one_hot(y, self.num_labels)
+        y = tf.squeeze(y)
+
+        x = tf.convert_to_tensor(input_dict["features"])
+        x = tf.nn.embedding_lookup(self.embedding_matrix, x)
+        x = self.encoder(x)
+        x = reparametrize(self.mu_layer(x), self.logvar_layer(x))
+        outputs = self.lstm(x)
+
+        logits = self.linear2(self.linear1(tf.cast(outputs, dtype=tf.float32)))
+        y_pred = tf.nn.softmax(logits)
+        loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+
+        return_dict = {"loss": loss, "y_pred": y_pred}
+        return return_dict
